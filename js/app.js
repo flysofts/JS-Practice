006e8c4e5fc4c932606e5ddb75b5d0f78fa35f68
flysofts <ds4950@naver.com>
2024-05-12 23:37:59 +0900

    JS Momentum

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+
+const HIDDEN_CLASSNAME = "hidden";
+// function onLoginSubmit(){
+//     const username = loginInput.value;
+//     console.log(username);
+// }
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // greeting.innerText = "hello " + username;
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+
+
+
+
